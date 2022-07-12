596b9d38bedde9ae5ac6a27b3983b871db46216e
Jefferson <jeferson.oliveira@g.globo>
2022-07-11 22:23:44 -0300

    criando o status da pagina de historico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 const History = () => {
   return (
@@ -18,19 +18,25 @@ const History = () => {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há certa de 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="yellow">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há certa de 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há certa de 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="red">Concluido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
